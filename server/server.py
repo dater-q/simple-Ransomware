1c7ccec10175caa3d80da576d037a858ea74fe74
dater-q <115425261+dater-q@users.noreply.github.com>
2023-03-28 17:44:52 +0100

    init
    just deposited everything

---
+++ server/server.py
@@ -0,0 +1,27 @@
+import socket
+
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(('localhost' , 9999))
+
+server.listen()
+
+while True:
+
+    client , addr = server.accept()
+
+    msg = client.recv(1024)
+
+    if msg == b"<RQST>":
+        print("req")
+        import sender
+    else:
+        file = open('thekey.key','wb')
+
+        key = msg
+
+        file.write(key)
+
+        file.close()
+        client.close()
+        #server.close()
